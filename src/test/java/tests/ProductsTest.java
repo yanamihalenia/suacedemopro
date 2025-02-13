7e090be03007cbd105171fff5e92f45273f471c3
yanamihalenia <yana.miga@gmail.com>
2025-02-13 22:06:03 +0300

    homework19 test case 1

--- src/test/java/tests/ProductsTest.java
+++ src/test/java/tests/ProductsTest.java
@@ -4,6 +4,8 @@ import constants.IConstants;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class ProductsTest extends BaseTest {
 
     @Test
@@ -20,5 +22,25 @@ public class ProductsTest extends BaseTest {
         loginPage.login(USERNAME,PASSWORD);
         productsPage.addProduct(SAUCE_LABS_BACKPACK);
         Assert.assertTrue(productsPage.isRemoveButtonDisplayed(SAUCE_LABS_BACKPACK));
+    }
+
+    @Test(description = "Test case 1: Check filter on products page")
+    public void checkFilterTest(){
+        List<String> listOfItems;
+        List<Double> listOfPrices;
+        loginPage.openPage(IConstants.LOGIN_PAGE_URL);
+        loginPage.login(USERNAME,PASSWORD);
+        productsPage.selectOptionInFilter(FILTER_FROM_A_TO_Z);
+        listOfItems = productsPage.getListOfItems();
+        productsPage.checkFiltrationFromAtoZ(listOfItems);
+        productsPage.selectOptionInFilter(FILTER_FROM_Z_TO_A);
+        listOfItems = productsPage.getListOfItems();
+        productsPage.checkFiltrationFromZtoA(listOfItems);
+        productsPage.selectOptionInFilter(FILTER_FROM_LOW_TO_HIGH_PRICE);
+        listOfPrices = productsPage.getListOfPrices();
+        productsPage.checkFiltrationFromLowToHigh(listOfPrices);
+        productsPage.selectOptionInFilter(FILTER_FROM_HIGH_TO_LOW_PRICE);
+        listOfPrices = productsPage.getListOfPrices();
+        productsPage.checkFiltrationFromHighToLow(listOfPrices);
     }
 }
