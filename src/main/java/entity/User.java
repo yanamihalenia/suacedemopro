4d21b3f10ed4ede9fe4d96a97a8d4c89cb2f5994
yanamihalenia <yana.miga@gmail.com>
2025-02-25 21:41:25 +0300

    changes from lesson

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,10 +1,14 @@
 package entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     String username;
     String password;
